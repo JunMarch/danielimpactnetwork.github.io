814ced70b24edb069dbc12a88a7e7318a4083fc1
JunMarch <127476106+JunMarch@users.noreply.github.com>
2023-05-10 00:21:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Program data
+/*const programs = [
+    {
+        name: "Program 1",
+        image: 
+ /*   },
+    {
+        name: "Program 2",
+        image: "program2.jpg"
+    },
+    {
+        name: "Program 3",
+/*        image: "program3.jpg"
+    }
+];
+
+// Add dynamic list items with images to the programs section
+/*const programsList = document.getElementById("programs-list");
+
+/*programs.forEach(program => {
+    const listItem = document.createElement("li");
+/*    const programImage = document.createElement("img");
+    programImage.src = program.image;
+    programImage.alt = program.name;
+    listItem.textContent = program.name;
+    listItem.appendChild(programImage);
+    programsList.appendChild(listItem);
+});
+*/
